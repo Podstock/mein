be9e102a3a7d0ef6b7116a30c017eb9242a449fb
Sebastian Reimers <hallo@studio-link.de>
2021-06-20 22:18:48 +0200

    fix random deadjokes

--- public/flachwitze.php
+++ public/flachwitze.php
@@ -1,15 +1,12 @@
 <?php
-$dir = dirname($_SERVER['DOCUMENT_ROOT']) . "/storage/app/flachwitze";
-$zufall = sprintf('%02d', mt_rand(1, 39));
-$file = $dir . "/" . $filename;
-
-$extension = "mp3";
-$mime_type = "audio/mpeg, audio/x-mpeg, audio/x-mpeg-3, audio/mpeg3";
+$dir = dirname($_SERVER['DOCUMENT_ROOT']) . "/storage/app/public/flachwitze";
+$random = sprintf('%02d', mt_rand(1, 39));
+$file = $dir . "/" . $random . '.mp3';
 
 if (file_exists($file)) {
-    header('Content-type: {$mime_type}');
+    header("Content-type: audio/mpeg, audio/x-mpeg, audio/x-mpeg-3, audio/mpeg3");
     header('Content-length: ' . filesize($file));
-    header('Content-Disposition: filename="' . $filename);
+    header('Content-Disposition: filename="flachwitz.mp3"');
     header('X-Pad: avoid browser bug');
     header('Cache-Control: no-cache');
     readfile($file);
