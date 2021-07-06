2b7f4c9a212b4d33a2266c905dd85e82c2e39572
Sebastian Reimers <hallo@studio-link.de>
2021-07-06 14:40:01 +0200

    page: better styling

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,7 +1,7 @@
 <x-empty-layout>
     <div class="max-w-screen-sm mx-auto p-5">
         <h1 class="text-3xl border-b mb-3 pb-1">{{$page->title}}</h1>
-        <div id="page_body">
+        <div class="prose lg:prose-xl">
         {!!$page->body!!}
         </div>
     </div>
