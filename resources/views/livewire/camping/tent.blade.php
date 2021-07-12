f50de14e0a009fc4a1a50f672f7eae51612aed40
Sebastian Reimers <hallo@studio-link.de>
2021-07-12 20:38:49 +0200

    update tent

--- resources/views/livewire/camping/tent.blade.php
+++ resources/views/livewire/camping/tent.blade.php
@@ -7,12 +7,12 @@
             <div
                 class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                 <div class="flex-shrink-0">
-                    <img class="h-10 w-10 rounded-full" src="/storage/{{$mytent->image}}" alt="">
+                    <img height="85" width="64" src="/storage/{{$mytent->image}}" alt="">
                 </div>
                 <div class="flex-1 min-w-0">
                     <span class="absolute inset-0" aria-hidden="true"></span>
                     <p class="text-sm font-medium text-gray-900">
-                        {{$mytent->description}}
+                        {!! Str::markdown($mytent->description) !!}
                     </p>
                 </div>
             </div>
@@ -32,7 +32,7 @@
         <div wire:click="select({{$tent->id}})" wire:key="{{ $tent->id }}"
             class="cursor-pointer relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
             <div class="flex-shrink-0">
-                <img class="h-10 w-10 rounded-full" src="/storage/{{$tent->image}}" alt="">
+                <img height="64px" width="48px" src="/storage/{{$tent->image}}" alt="">
             </div>
             <div class="flex-1 min-w-0">
                 <span class="absolute inset-0" aria-hidden="true"></span>
