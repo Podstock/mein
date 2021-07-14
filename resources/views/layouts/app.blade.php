62305dbdda716dfa9683c972166dece286475732
Sebastian Reimers <hallo@studio-link.de>
2021-07-14 08:34:24 +0200

    add favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,17 @@
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <!-- PWA -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=yyL6zP5339">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=yyL6zP5339">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=yyL6zP5339">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=yyL6zP5339" color="#81a540">
+    <link rel="shortcut icon" href="/favicon.ico?v=yyL6zP5339">
+    <meta name="apple-mobile-web-app-title" content="Mein Podstock">
+    <meta name="application-name" content="Mein Podstock">
+    <meta name="msapplication-TileColor" content="#81a540">
+    <meta name="theme-color" content="#ffffff">
 </head>
 
 <body class="font-sans antialiased bg-gray-100">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,18 @@
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
+
+
+    <!-- PWA -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=yyL6zP5339">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=yyL6zP5339">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=yyL6zP5339">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=yyL6zP5339" color="#81a540">
+    <link rel="shortcut icon" href="/favicon.ico?v=yyL6zP5339">
+    <meta name="apple-mobile-web-app-title" content="Mein Podstock">
+    <meta name="application-name" content="Mein Podstock">
+    <meta name="msapplication-TileColor" content="#81a540">
+    <meta name="theme-color" content="#ffffff">
 </head>
 
 <body class="bg-gray-100">
@@ -26,8 +38,8 @@
         </div>
         <div class="flex justify-center text-sm text-grey mt-2">
             <a class="no-underline text-grey-darker hover:underline px-1"
-                href="https://www.podstock.de/code_of_conduct.html" target="_blank" rel="noopener">Verhaltenskodex</a><span
-                aria-hidden="true"> | </span>
+                href="https://www.podstock.de/code_of_conduct.html" target="_blank"
+                rel="noopener">Verhaltenskodex</a><span aria-hidden="true"> | </span>
             <a class="no-underline text-grey-darker hover:underline px-1"
                 href="https://www.podstock.de/datenschutz.html" target="_blank" rel="noopener">Datenschutz</a><span
                 aria-hidden="true"> | </span>
