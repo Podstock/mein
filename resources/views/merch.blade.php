d60220cec4e367e9b4ac7e4930c862aafa17d8d3
Sebastian Reimers <hallo@studio-link.de>
2021-07-14 08:08:42 +0200

    finalize merch page

--- resources/views/merch.blade.php
+++ resources/views/merch.blade.php
@@ -11,35 +11,32 @@
                 <h2 class="text-2xl mb-2">Sticker/Aufkleber einsenden</h2>
                 <div class="max-w-7xl px-6 pb-6 bg-white shadow-md overflow-hidden rounded-lg prose">
                     <div class="text-center font-bold">
-                    <p class="text-lg">Bis zum 31.07.2021 kannst du uns <br />unter folgender Adresse Sticker schicken:</p>
+                        <p class="text-lg">Bis zum 31.07.2021 kannst du uns <br />unter folgender Adresse Sticker
+                            schicken:</p>
                         Ralph Meyer<br />
                         Postnummer 34144422<br />
                         Packstation 103<br />
                         74172 Neckarsulm<br />
-                                            <a href="https://www.deutschepost.de/de/b/briefe-adressieren-an-packstation.html"
-                        target="_blank">Versandbedingungen
-                        der Post</a>
-                       <p>Kalkuliere am besten mit einer Menge zwischen 50 und max. 300 Stickern. <br />Bitte keine Buttons oder anderer Merch.</p>
+                        <a href="https://www.deutschepost.de/de/b/briefe-adressieren-an-packstation.html"
+                            target="_blank">Versandbedingungen
+                            der Post (Packstation)</a>
+                        <p>Kalkuliere am besten mit einer Menge zwischen 50 und max. 300 Stickern. <br />Bitte keine
+                            Buttons oder anderer Merch.</p>
                     </div>
-                    
+
                 </div>
             </div>
 
             <div>
                 <h2 class="text-2xl mb-2">Sticker Paket bestellen</h2>
-                <div class="max-w-7xl p-6 bg-white shadow-md overflow-hidden rounded-lg prose">
-                    @php
-                    echo Str::markdown("
-Demnächst kannst du hier dein Festival Sticker Paket bestellen. Enthalten sein wird:
-- Das Podstock 2021 Festivalbändchen
-- mind. 2x Podstock Sticker
-- Eine Mischung aus allen bis dahin eingesendeten Stickern
+                <div class="text-center">
+                    <a href="https://tickets.podstock.de/podstock/2021-sticker/" target="_blank">
 
-
-Preis: **5 Euro** inkl. 19% MwSt.
-
-                    ");
-                    @endphp
+                        <img src="/images/sticker.png" class="mx-auto mb-4 md:w-2/3" aria-hidden="true" />
+                        <div
+                        class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-2xl font-medium rounded-md text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+                        Zum Shop</div>
+                    </a>
                 </div>
             </div>
 
