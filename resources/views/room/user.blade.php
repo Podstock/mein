d6b8169ecd488b88398088f75d81b5f86c8650f1
Sebastian Reimers <hallo@studio-link.de>
2021-08-07 12:40:00 +0200

    fix is_moderator check

--- resources/views/room/user.blade.php
+++ resources/views/room/user.blade.php
@@ -7,7 +7,7 @@
         <div class="flex flex-col text-center">
             <img class="mx-auto h-16 w-16 rounded-full lg:w-20 lg:h-20 mb-4" src="{{$user?->ProfilePhotoUrl}}" alt="" />
 
-            @if($user?->is_moderator($room->id))
+            @if(auth()->user()->is_moderator($room->id))
             @if($user?->is_speaker($room->id))
             <button wire:click="makeListener()" type="button"
                 class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-700">
