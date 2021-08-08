680478db7a796bb6966abc6998504a4b57059bb6
Sebastian Reimers <hallo@studio-link.de>
2021-08-08 15:27:59 +0200

    add default target _blank for pages

---
+++ resources/views/layouts/page.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    @livewireStyles
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    @auth
+    <script>
+        window.user_id = {{ auth()->user()->id }}
+    </script>
+    @endauth
+
+    <!-- PWA -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=yyL6zP5339">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=yyL6zP5339">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=yyL6zP5339">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=yyL6zP5339" color="#81a540">
+    <link rel="shortcut icon" href="/favicon.ico?v=yyL6zP5339">
+    <meta name="apple-mobile-web-app-title" content="Mein Podstock">
+    <meta name="application-name" content="Mein Podstock">
+    <meta name="msapplication-TileColor" content="#81a540">
+    <meta name="theme-color" content="#ffffff">
+    <base target="_blank">
+</head>
+
+<body class="bg-gray-100">
+    <div class="font-sans text-gray-900 antialiased">
+        {{ $slot }}
+    </div>
+    @livewireScripts
+</body>
+
+</html>
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,8 +1,8 @@
-<x-empty-layout>
+<x-page-layout>
     <div class="max-w-screen-sm mx-auto p-5">
         <h1 class="text-3xl border-b mb-3 pb-1">{{$page->title}}</h1>
         <div class="prose lg:prose-xl">
         {!!$page->body!!}
         </div>
     </div>
-</x-empty-layout>
+</x-page-layout>
