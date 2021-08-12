bff01e284bb70c20d15bad616133a71e7004aa13
Sebastian Reimers <hallo@studio-link.de>
2021-08-12 20:06:07 +0200

    dashboard wording

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
             <a href="/user/workadventure/login"
                 class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-lg font-bold rounded-md text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Zum
-                Lasttest</a>
+                Warmup - Wartebereich</a>
         </div>
     </div>
 </x-app-layout>
