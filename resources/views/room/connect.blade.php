6c8e9983412f0245cefe49398d2ad912fd325027
Sebastian Reimers <hallo@studio-link.de>
2021-08-14 07:26:15 +0200

    add stream link on connect page if show

--- resources/views/room/connect.blade.php
+++ resources/views/room/connect.blade.php
@@ -6,7 +6,7 @@
     <x-slot name="content">
         <div>
             <dl
-                class="mt-5 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 sm:grid-cols-3 sm:divide-y-0 sm:divide-x">
+                class="mt-5 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200">
                 {{-- WebRTC --}}
                 <button wire:click="webrtc" type="button" class="px-4 p-5 hover:bg-gray-300 flex flex-col">
                     <dt class="text-base font-bold text-gray-900 text-center mb-2 w-full">
@@ -26,29 +26,9 @@
                         </div>
                     </dd>
                 </button>
-                {{-- Livestream Audio --}}
-                <button type="button" class="px-4 p-5 hover:bg-gray-300 flex flex-col opacity-30">
-                    <dt class="text-base font-bold text-gray-900 text-center mb-2 w-full">
-                        Audio Livestream
-                    </dt>
-                    <dd class="mt-1 flex flex-col w-full">
-
-                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="waveform-lines"
-                            class="h-8 w-8 sm:h-12 sm:w-12 mx-auto" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 640 512">
-                            <path fill="currentColor"
-                                d="M224 96C206.3 96 192 110.3 192 127.1v256C192 401.7 206.3 416 223.1 416S256 401.7 256 384V127.1C256 110.3 241.7 96 224 96zM32 224C14.33 224 0 238.3 0 255.1S14.33 288 31.1 288S64 273.7 64 256S49.67 224 32 224zM320 0C302.3 0 288 14.33 288 31.1V480C288 497.7 302.3 512 319.1 512S352 497.7 352 480V31.1C352 14.33 337.7 0 320 0zM128 192C110.3 192 96 206.3 96 223.1V288C96 305.7 110.3 320 127.1 320S160 305.7 160 288V223.1C160 206.3 145.7 192 128 192zM608 224c-17.67 0-32 14.33-32 31.1S590.3 288 608 288s32-14.33 32-31.1S625.7 224 608 224zM416 128C398.3 128 384 142.3 384 159.1v192C384 369.7 398.3 384 415.1 384S448 369.7 448 352V159.1C448 142.3 433.7 128 416 128zM512 64c-17.67 0-32 14.33-32 31.1V416C480 433.7 494.3 448 511.1 448C529.7 448 544 433.7 544 416V95.1C544 78.33 529.7 64 512 64z">
-                            </path>
-                        </svg>
-
-                        <div class="text-center w-full mt-4 text-sm text-gray-500">
-                            Falls du nur zuhören möchtest und Latenz nicht so wichtig ist.
-                        </div>
-                    </dd>
-                </button>
-
+                @if($room->show)
                 {{-- Livestream HLS/VIDEO --}}
-                <button type="button" class="px-4 p-5 hover:bg-gray-300 flex flex-col opacity-30">
+                <a href="/stream/{{$room->slug}}" class="px-4 p-5 hover:bg-gray-300 flex flex-col">
                     <dt class="text-base font-bold text-gray-900 text-center mb-2 w-full">
                         Video Livestream
                     </dt>
@@ -64,7 +44,8 @@
                             Wenn Bandbreite und Latenz keine Rolle spielen (nicht alle Streams haben Videoinhalte).
                         </div>
                     </dd>
-                </button>
+                </a>
+                @endif
             </dl>
         </div>
 
